1dfae6b3566c7f2859bacb6dd625aec9461d5a55
yevkk <yevkk@users.noreply.github.com>
2019-09-28 15:21:10 +0300

    Added add_node() method

--- lab1/README.md
+++ lab1/README.md
@@ -3,6 +3,14 @@
 Graphs based on adjacency structure and adjacency matrix, that can store any data type
 
 **Methods implemented:**
+- [x] add node
+- [ ] add edge
+- [ ] find node by value
+- [ ] delete node by index
+- [ ] delete node by value
+- [ ] delete edge
+- [ ] check adjacency
+- [ ] distance between nodes (by index)
 
 ### Custom class
 A dice with face number N = 2, 4, 6, 8, 12, 20.
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,9 +4,10 @@
 template<typename T, typename M>
 class Graph {
 public:
+    virtual void add_node(T data) = 0;
 };
 
-template <typename T>
+template<typename T>
 class GraphEdge {
 public:
     T data;
@@ -16,7 +17,7 @@ public:
     }
 };
 
-template <typename T>
+template<typename T>
 class GraphNode {
 public:
     T data;
@@ -28,19 +29,19 @@ public:
     }
 };
 
-
 template<typename T, typename M>
-class GraphNodeAdjStr : public GraphNode<T>{
+class GraphNodeAdjStr : public GraphNode<T> {
 public:
-    std::vector<std::pair<int, GraphEdge<M>*>> adjacent_nodes;
+    std::vector<std::pair<int, GraphEdge<M> *>> adjacent_nodes;
 
-    explicit GraphNodeAdjStr(int Data){
+    explicit GraphNodeAdjStr(int Data) {
         this->data = Data;
     }
 };
 
 template<typename T, typename M>
 class GraphAdjStr : public Graph<T, M> {
+public:
     std::vector<GraphNodeAdjStr<T, M> *> nodes;
     bool directed;
 
@@ -51,12 +52,17 @@ class GraphAdjStr : public Graph<T, M> {
     ~GraphAdjStr() {
         for (auto &node:nodes) delete node;
     }
+
+    void add_node(T data) override {
+        nodes.push_back(new GraphNodeAdjStr<T, M>(data));
+    }
 };
 
 template<typename T, typename M>
-class GraphMtrx : public Graph<T, M>{
-    std::vector<GraphNode<T>*> nodes;
-    std::vector<std::vector<std::pair<bool, GraphEdge<M>*>>> edges;
+class GraphMtrx : public Graph<T, M> {
+public:
+    std::vector<GraphNode<T> *> nodes;
+    std::vector<std::vector<std::pair<bool, GraphEdge<M> *>>> edges;
     bool directed;
 
     explicit GraphMtrx(bool Directed = false) {
@@ -69,11 +75,22 @@ class GraphMtrx : public Graph<T, M>{
             for (int i = 0; i < edge.size(); i++)
                 delete edge[i].second;
     }
+
+    void add_node(T data) override {
+        nodes.push_back(new GraphNodeAdjStr<T, M>(data));
+    }
 };
+
+void test_int(Graph<int, int> *graph) {
+    graph->add_node(5);
+}
 
 
 int main() {
-
+    auto G1 = new GraphAdjStr<int, int>();
+    test_int(G1);
+    auto G2 = new GraphMtrx<int, int>();
+    test_int(G2);
 
     return 0;
 }
