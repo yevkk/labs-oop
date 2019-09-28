7cad17d37ba3b6b91f6f6c3c7f27b4b5742d7260
yevkk <yevkk@users.noreply.github.com>
2019-09-28 12:15:12 +0300

    Declared classes

--- lab1/README.md
+++ lab1/README.md
@@ -6,6 +6,6 @@ Graphs based on adjacency structure and adjacency matrix, that can store any dat
 
 ### Custom class
 A dice with face number N = 2, 4, 6, 8, 12, 20.
-Every face has own probability of being dropped/
+Every face has own probability of being dropped.
 
 **Methods implemented:**
--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,6 +1,79 @@
 #include <iostream>
+#include <vector>
+
+template<typename T, typename M>
+class Graph {
+public:
+};
+
+template <typename T>
+class GraphEdge {
+public:
+    T data;
+
+    explicit GraphEdge(const T Data) {
+        data = Data;
+    }
+};
+
+template <typename T>
+class GraphNode {
+public:
+    T data;
+
+    GraphNode() = default;
+
+    explicit GraphNode(const T Data) {
+        data = Data;
+    }
+};
+
+
+template<typename T, typename M>
+class GraphNodeAdjStr : public GraphNode<T>{
+public:
+    std::vector<std::pair<int, GraphEdge<M>*>> adjacent_nodes;
+
+    explicit GraphNodeAdjStr(int Data){
+        this->data = Data;
+    }
+};
+
+template<typename T, typename M>
+class GraphAdjStr : public Graph<T, M> {
+    std::vector<GraphNodeAdjStr<T, M> *> nodes;
+    bool directed;
+
+    explicit GraphAdjStr(bool Directed = false) {
+        directed = Directed;
+    }
+
+    ~GraphAdjStr() {
+        for (auto &node:nodes) delete node;
+    }
+};
+
+template<typename T, typename M>
+class GraphMtrx : public Graph<T, M>{
+    std::vector<GraphNode<T>*> nodes;
+    std::vector<std::vector<std::pair<bool, GraphEdge<M>*>>> edges;
+    bool directed;
+
+    explicit GraphMtrx(bool Directed = false) {
+        directed = Directed;
+    }
+
+    ~GraphMtrx() {
+        for (auto &node:nodes) delete node;
+        for (auto &edge:edges)
+            for (int i = 0; i < edge.size(); i++)
+                delete edge[i].second;
+    }
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
     return 0;
 }
