b6405bd63a31e2711d8f2c6a88460fc12154d842
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-07-24 12:01:41 +0300

    classes' definitions

--- lab2-sem2/src/BTree.hpp
+++ lab2-sem2/src/BTree.hpp
@@ -1,1 +1,62 @@
 #pragma once
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <type_traits>
+
+template<typename DataType>
+class BTreePlain;
+
+template<typename DataType>
+class BTreePlus;
+
+template<typename DataType>
+class BTreeNode {
+public:
+
+protected:
+    std::vector<DataType> _keys;
+    std::vector<std::shared_ptr<BTreeNode>> _children;
+    bool _is_leaf;
+    std::weak_ptr<BTreeNode> _parent;
+
+};
+
+template<typename DataType>
+class BTreePlainNode : public BTreeNode<DataType> {
+    friend BTreePlain<DataType>;
+};
+
+class BTreePlusNode : public BTreeNode<std::size_t> {
+private:
+    std::weak_ptr<BTreePlusNode> _next_leaf;
+    std::weak_ptr<BTreePlusNode> _prev_leaf;
+
+    template<typename DataType>
+    friend
+    class BTreePlus;
+};
+
+template<
+        typename DataType,
+        typename NodeType,
+        typename = typename std::enable_if_t<std::is_base_of<BTreeNode<DataType>, NodeType>::value>
+>
+class BTree {
+protected:
+    std::size_t _min_degree;
+    std::shared_ptr<NodeType> _root;
+};
+
+template<typename DataType>
+class BTreePlain : public BTree<DataType, BTreePlainNode<DataType>> {
+    using Node = BTreePlainNode<DataType>;
+};
+
+template<typename DataType>
+class BTreePlus : public BTree<DataType, BTreePlusNode> {
+    using Node = BTreePlusNode;
+};
+
+#include "BTree.hxx"
