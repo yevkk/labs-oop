14d47097c2f36633e31b837968bdfde4301a8b50
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-07-24 21:09:54 +0300

    getters

--- lab2-sem2/src/BTree.hpp
+++ lab2-sem2/src/BTree.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <memory>
 #include <vector>
 #include <type_traits>
@@ -18,6 +17,37 @@ class BTreePlus;
 template<typename DataType>
 class BTreeNode {
 public:
+    /**
+     * @return number of keys stored in node
+     */
+    [[nodiscard]] std::size_t size() const;
+
+    /**
+     * @return number of node's children
+     */
+    [[nodiscard]] std::size_t childrenNumber() const;
+
+    /**
+     * @param index
+     * @return value of key stored at position index
+     */
+    [[nodiscard]] DataType getKeyValue(std::size_t index) const;
+
+    /**
+     * @param index
+     * @return shared_ptr to child node at position index
+     */
+    [[nodiscard]] std::shared_ptr<BTreeNode> getChild(std::size_t index) const;
+
+    /**
+     * @return true if node is leaf, else - false
+     */
+    [[nodiscard]] bool isLeaf() const;
+
+    /**
+     * @return shared_ptr to parent node
+     */
+    [[nodiscard]] std::shared_ptr<BTreeNode> parent() const;
 
 protected:
     std::vector<DataType> _keys;
@@ -41,6 +71,16 @@ class BTreePlainNode : public BTreeNode<DataType> {
  * @note stores keys of std::size_t type which correspond to indexes of values stored in member _data of BTreePlus object
  */
 class BTreePlusNode : public BTreeNode<std::size_t> {
+public:
+    /**
+     * @return shared_ptr to next leaf node
+     */
+    [[nodiscard]] std::shared_ptr<BTreePlusNode> nextLeaf() const;
+
+    /**
+     * @return shared_ptr to previous leaf node
+     */
+    [[nodiscard]] std::shared_ptr<BTreePlusNode> prevLeaf() const;
 private:
     std::weak_ptr<BTreePlusNode> _next_leaf;
     std::weak_ptr<BTreePlusNode> _prev_leaf;
@@ -62,8 +102,21 @@ template<
         typename = typename std::enable_if_t<std::is_base_of<BTreeNode<DataType>, NodeType>::value>
 >
 class BTree {
+public:
+    /**
+     * @return min degree parameter of tree
+     */
+    [[nodiscard]] std::size_t minDegree();
+
+    /**
+     * @return shared_ptr to root node of tree
+     */
+    [[nodiscard]] std::shared_ptr<NodeType> root() const;
+
 protected:
     std::size_t _min_degree;
+    std::size_t _min_node_fill;
+    std::size_t _max_node_fill;
     std::shared_ptr<NodeType> _root;
 };
 
@@ -85,6 +138,13 @@ class BTreePlain : public BTree<DataType, BTreePlainNode<DataType>> {
 template<typename DataType>
 class BTreePlus : public BTree<DataType, BTreePlusNode> {
     using Node = BTreePlusNode;
+public:
+    /**
+     * @param index
+     * @return value stored at position index in data container
+     */
+    DataType getValue(std::size_t index) const;
+
 private:
     /**
      * @brief stores keys of tree to avoid duplication of objects in non-leaf nodes
--- lab2-sem2/src/BTree.hxx
+++ lab2-sem2/src/BTree.hxx
@@ -1,1 +1,64 @@
 #pragma once
+
+#include <memory>
+#include <vector>
+#include <type_traits>
+#include <cassert>
+
+template<typename DataType>
+std::size_t BTreeNode<DataType>::size() const {
+    return _keys.size();
+}
+
+template<typename DataType>
+std::size_t BTreeNode<DataType>::childrenNumber() const {
+    return _children.size();
+}
+
+template<typename DataType>
+DataType BTreeNode<DataType>::getKeyValue(std::size_t index) const {
+    assert(index < size() && "wrong parameter value");
+    return _keys[index];
+}
+
+template<typename DataType>
+std::shared_ptr<BTreeNode<DataType>> BTreeNode<DataType>::getChild(std::size_t index) const {
+    assert(index < childrenNumber() && "wrong parameter value");
+    return _children[index];
+}
+
+template<typename DataType>
+bool BTreeNode<DataType>::isLeaf() const {
+    return _is_leaf;
+}
+
+template<typename DataType>
+std::shared_ptr<BTreeNode<DataType>> BTreeNode<DataType>::parent() const {
+    return _parent.lock();
+}
+
+std::shared_ptr<BTreePlusNode> BTreePlusNode::nextLeaf() const {
+    assert(_is_leaf && "called for non-leaf node");
+    return _next_leaf.lock();
+}
+
+std::shared_ptr<BTreePlusNode> BTreePlusNode::prevLeaf() const {
+    assert(_is_leaf && "called for non-leaf node");
+    return _prev_leaf.lock();
+}
+
+template<typename DataType, typename NodeType, typename Condition>
+std::size_t BTree<DataType, NodeType, Condition>::minDegree() {
+    return _min_degree;
+}
+
+template<typename DataType, typename NodeType, typename Condition>
+std::shared_ptr<NodeType> BTree<DataType, NodeType, Condition>::root() const {
+    return _root;
+}
+
+template<typename DataType>
+DataType BTreePlus<DataType>::getValue(std::size_t index) const {
+    assert(index < _data.size() && "wrong parameter value");
+    return _data[index];
+}
