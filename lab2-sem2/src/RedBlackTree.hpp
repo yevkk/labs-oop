fdac39a14eb4823310e07dc36577dbf9a115792d
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-08-08 22:16:40 +0300

    implementation of some rb-tree methods
    transplant, minNode, maxNode, successor, predecessor

--- lab2-sem2/src/RedBlackTree.hpp
+++ lab2-sem2/src/RedBlackTree.hpp
@@ -59,21 +59,51 @@ private:
 
     void _deleteFixup(std::shared_ptr<Node> node);
 
+    /**
+     * @brief replaces dst node with src
+     * @param dst destination node (with all its branches)
+     * @param src source node
+     */
     void _transplant(std::shared_ptr<Node> dst, std::shared_ptr<Node> src);
 
-    std::shared_ptr<Node> _subtreeMinNode(std::shared_ptr<Node> subtree_root);
+    /**
+     * @brief finds a node with min value in subtree
+     * @param subtree_root root of subtree being processed
+     * @return a node with min value in subtree with given root
+     */
+    auto _subtreeMinNode(std::shared_ptr<Node> subtree_root) -> std::shared_ptr<Node>;
 
-    std::shared_ptr<Node> _subtreeMaxNode(std::shared_ptr<Node> subtree_root);
+    /**
+     * @brief finds a node with max value in subtree
+     * @param subtree_root root of subtree being processed
+     * @return a node with max value in subtree with given root
+     */
+    auto _subtreeMaxNode(std::shared_ptr<Node> subtree_root) -> std::shared_ptr<Node>;
 
-    std::shared_ptr<Node> _nodeSuccessor(std::shared_ptr<Node> node);
+    /**
+     * @brief finds a node with successor value
+     * @param node a node of tree
+     * @return a node with value, that is a successor of value in given node
+     */
+    auto _nodeSuccessor(std::shared_ptr<Node> node) -> std::shared_ptr<Node>;
 
-    std::shared_ptr<Node> _nodePredecessor(std::shared_ptr<Node> node);
+    /**
+     * @brief finds a node with predecessor value
+     * @param node a node of tree
+     * @return a node with value? that is a predecessor of value in given node
+     */
+    auto _nodePredecessor(std::shared_ptr<Node> node) -> std::shared_ptr<Node>;
 
-    std::shared_ptr<Node> _searchImpl(std::shared_ptr<Node> node, const value_type &key);
+    auto _searchImpl(std::shared_ptr<Node> node, const value_type &key) -> std::shared_ptr<Node>;
 
     void _leftRotate(std::shared_ptr<Node> node);
 
     void _rightRotate(std::shared_ptr<Node> node);
+
+    std::shared_ptr<Node> _root;
+    std::shared_ptr<Node> _null_node;
 };
+
+
 
 #include "RedBlackTree.hxx"
--- lab2-sem2/src/RedBlackTree.hxx
+++ lab2-sem2/src/RedBlackTree.hxx
@@ -19,3 +19,88 @@ void RedBlackTree<T>::Node::switchColor() {
     };
 }
 
+template<typename T>
+RedBlackTree<T>::RedBlackTree() :
+        _root{nullptr},
+        _null_node{std::make_shared<Node>(value_type{})} {
+    _null_node->color = Node::Color::BLACK;
+}
+
+template<typename T>
+void RedBlackTree<T>::_insertFixup(std::shared_ptr<Node> node) {
+
+}
+
+template<typename T>
+void RedBlackTree<T>::_deleteFixup(std::shared_ptr<Node> node) {
+
+}
+
+template<typename T>
+void RedBlackTree<T>::_transplant(std::shared_ptr<Node> dst,
+                                  std::shared_ptr<Node> src) {
+    if (dst->parent.lock() == _null_node) {
+        _root = src;
+    } else if (dst == dst->parent.lock()->left) {
+        dst->parent.lock()->left = src;
+    } else {
+        dst->parent.lock()->right = src;
+    }
+
+    if (src != _null_node) {
+        src->parent = dst->parent;
+    }
+}
+
+template<typename T>
+auto RedBlackTree<T>::_subtreeMinNode(std::shared_ptr<Node> subtree_root) -> std::shared_ptr<Node> {
+    auto ptr = _root;
+
+    while (ptr->left != _null_node) {
+        ptr = ptr->left;
+    }
+
+    return ptr;
+}
+
+template<typename T>
+auto RedBlackTree<T>::_subtreeMaxNode(std::shared_ptr<Node> subtree_root) -> std::shared_ptr<Node> {
+    auto ptr = _root;
+
+    while (ptr->right != _null_node) {
+        ptr = ptr->right;
+    }
+
+    return ptr;
+}
+
+template<typename T>
+auto RedBlackTree<T>::_nodeSuccessor(std::shared_ptr<Node> node) -> std::shared_ptr<Node> {
+    if (node->right != _null_node) {
+        return _subtreeMinNode(node->right);
+    }
+
+    auto ptr = node->parent.lock();
+    while((ptr != _null_node) && (node == ptr->right)) {
+        node = ptr;
+        ptr = ptr->parent.lock();
+    }
+
+    return ptr;
+}
+
+template<typename T>
+auto RedBlackTree<T>::_nodePredecessor(std::shared_ptr<Node> node) -> std::shared_ptr<Node> {
+    if (node->left != _null_node) {
+        return _subtreeMaxNode(node->left);
+    }
+
+    auto ptr = node->parent.lock();
+    while((ptr != _null_node) && (node == ptr->left)) {
+        node = ptr;
+        ptr = ptr->parent.lock();
+    }
+
+    return ptr;
+}
+
