71f3b43d6bca1ebec25af0c759ce1a5234bac3d4
Yevgeniy Kutsenko <47559940+yevkk@users.noreply.github.com>
2020-08-30 23:00:40 +0300

    benchmark core

---
+++ lab3a-sem2/benchmark/benchmark.cpp
@@ -0,0 +1,16 @@
+#include <benchmark/benchmark.h>
+
+#include "../src/Matrix.hpp"
+
+Matrix<int> randomSquareIntMatrix(const std::size_t &size) {
+
+}
+
+static void BM_default_multiplication(benchmark::State &state) {
+
+
+    for (auto _ : state) {
+
+    }
+}
+
